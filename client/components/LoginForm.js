9b3dce5d71c818db055db1dcbcd48ee4caaa0e40
Shayne Meyer <shayne.meyer@gmail.com>
2018-01-25 15:44:09 -0800

    initial commit

---
+++ client/components/LoginForm.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import AuthForm from './AuthForm';
+import mutation from '../mutations/Login';
+import { graphql } from 'react-apollo';
+import query from '../queries/CurrentUser';
+
+class LoginForm extends Component {
+  onSubmit({ email, password}) {
+    this.props.mutate({
+      variables: { email, password },
+      refetchQueries: [{ query }]
+    }).catch(res => {
+      const errors = res.graphQLErrors.map(error => error.message);
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Login</h3>
+        <AuthForm onSubmit={this.onSubmit.bind(this)}/>
+      </div>
+    );
+  }
+}
+
+export default graphql(mutation)(LoginForm);
